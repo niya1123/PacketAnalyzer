eeb46c8eab6bc384eef411e4c06ca52b000c9b61
niya1123 <yoheikurioka@gmail.com>
2019-12-29 10:56:51 +0900

    docの追加

--- packet_analyzer/lib/packet_analyzer.ex
+++ packet_analyzer/lib/packet_analyzer.ex
@@ -7,19 +7,34 @@ defmodule PacketAnalyzer do
   if it comes from the database, an external API or others.
   """
 
+  @doc """
+  入力されたデータを返します.
+
+  ## パラメータ
+    - input_data: 入力文字列です.
+  
+  ## try-rescue
+    input_dataに16進数以外の文字列が与えられた場合にエラーが出るようにしています.
+    ただし, 十分な長さがない16進数の文字列でも通ってしまう.
+  """
   def analyze(input_data) do
     try do
       input_data
       |> String.to_integer(16)
+      |> Integer.to_string(16)
     rescue
-      e in ArgumentError -> "Error"
-      e in RuntimeError -> "Error"
+      e -> "Error"
     end
   end
 
+  @doc """
+  バージョンを返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def version(analyze) do
     version = analyze
-    |> Integer.to_string(16)
     |> String.first
 
     case version do
@@ -29,60 +44,106 @@ defmodule PacketAnalyzer do
       "7" -> version<>" -> TP/IX"
       "8" -> version<>" -> PIP"
       "9" -> version<>" -> TUBA"
+       _  -> "Not Match version"
     end
   end
 
+  @doc """
+  ヘッダ長を返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def header(analyze) do
     header = analyze
-    |> Integer.to_string(16)
     |> String.at(1)
   end
 
+  @doc """
+  サービスタイプを返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def service(analyze) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 2,2
   end
 
+  @doc """
+  パケット長を返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def packet_length(analyze) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 4,4
   end
 
+  @doc """
+  識別子を返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def identifier(analyze) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 8,4
   end
 
+  @doc """
+  フラグを返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def flag(analyze) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 12,4
   end
 
+  @doc """
+  TTLを返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def ttl(analyze) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 16,2
   end
 
+  @doc """
+  プロトコル番号を返します.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def procotol(analyze) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 18,2
   end
 
+  @doc """
+  ヘッダチェックサムを返す.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def header_check(analyze) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 20,4
   end
 
+  @doc """
+  送信元IPアドレスを返す.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def source_ip_addr(analyze) do
     ip = analyze
-    |> Integer.to_string(16)
     |> String.slice 24,8
 
     ip_1 = String.slice(ip,0,2) |> String.to_integer(16) |> Integer.to_string
@@ -93,9 +154,14 @@ defmodule PacketAnalyzer do
     ip<>" -> "<>ip_1<>"."<>ip_2<>"."<>ip_3<>"."<>ip_4
   end
 
+  @doc """
+  宛先IPアドレスを返す.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def destination_ip_addr(analyze) do
     ip = analyze
-    |> Integer.to_string(16)
     |> String.slice 32,8
 
     ip_1 = String.slice(ip,0,2) |> String.to_integer(16) |> Integer.to_string
@@ -106,15 +172,25 @@ defmodule PacketAnalyzer do
     ip<>" -> "<>ip_1<>"."<>ip_2<>"."<>ip_3<>"."<>ip_4
   end
 
+  @doc """
+  オプションを返す.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def option(analyze, n) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice 40,8*n
   end
 
+  @doc """
+  データを返す.
+
+  ## パラメータ
+    - analyze: analyzeで返ってきた文字列.
+  """
   def data(analyze, n) do
     analyze
-    |> Integer.to_string(16)
     |> String.slice (40+8*n)..-1
   end
   
